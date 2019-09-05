19d58f4f1ac563c6a6bd4f36879c9a3c84e82fea
louluya <louly@rd.netease.com>
2019-09-05 11:10:18 +0800

    ee

--- src/moduleA.js
+++ src/moduleA.js
@@ -1,4 +1,4 @@
-var newDiv = document.createElement('div') 
-var newContent = document.createTextNode("Hi there! I'm module A!")
+var newDiv = document.createElement('div')
+var newContent = document.createTextNode("Hi there! I'm moduddddddle A!")
 newDiv.appendChild(newContent)
 document.body.appendChild(newDiv)
